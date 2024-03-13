083295ee7c3f6d4782de39d936e4979475907e7c
Sajana Rupasinghe <navodyasajana234@gmail.com>
2024-03-13 20:40:50 +0530

    sajana/Splash_view

---
+++ clothing-app/Views/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  clothing-app
+//
+//  Created by Sajana Rupasinghe on 2024-03-13.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text("Clothing app Splash Screen\nHello There!")
+    }
+}
+
+#Preview {
+    SplashView()
+}
