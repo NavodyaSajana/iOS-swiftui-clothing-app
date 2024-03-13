6eafba0d6829e31b74b4d564c75dea142029fc2f
Sajana Rupasinghe <navodyasajana234@gmail.com>
2024-03-13 20:17:02 +0530

    Initial Commit

---
+++ clothing-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  clothing-app
+//
+//  Created by Sajana Rupasinghe on 2024-03-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
